1c0dfc44e719074a11304de106759893922fe546
Paul <paul@gomera.fr>
2018-06-17 16:57:03 +0200

    Lien vers la liste des épisodes v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   get 'series/index'
+  get 'series/episodes'
 
   devise_for :users
   resources :marks
   resources :seen_episode_series
   resources :current_watch_series
   resources :episodes
-  resources :series
+  resources :series, only: [:show]
   resources :users
-  get 'welcome/index'
+  # get 'welcome/index'
   root :to => "series#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
